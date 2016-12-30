edfdc0c4362157efe8195e4849cd89cad90f5164
agnewee <agnewee@163.com>
2016-12-30 13:47:18 +0800

    合并Dev分支

---
+++ bdy/baiduyun.py
@@ -0,0 +1,99 @@
+# coding: utf-8
+##
+# @file baiduyun.py
+# @brief 
+# @author scusjs@foxmail.com
+# @version 0.1.00
+# @date 2016-12-28
+from __future__ import print_function
+import sys
+import json
+import requests
+from . import oauth_ui
+from . import utils
+
+if sys.version_info[0] < 3:
+    str = unicode
+    bytes = str
+
+
+class BaiduYun(object):
+    def __init__(self, oauth_info):
+        self.oauth_info = oauth_info
+        self.base_url = utils.get_config("base_url")
+        self.base_path = utils.get_config("bash_path")
+
+    def space_info(self):
+        params = {"method": "info",
+                  "access_token": oauth_info['access_token']}
+        response = requests.get(utils.get_config('quota_url'),
+                                params=params)
+        result = json.loads(response.content)
+        if 'error_code' in result:
+            return False, result
+        return True, result
+
+    def list_dir(self, path="/", method='list', by='time'):
+        path = self.base_path + path
+        params = {"method": method,
+                  "access_token": self.oauth_info['access_token'],
+                  "by": by,
+                  "path": path}
+        response = requests.get(self.base_url,
+                                params=params)
+        result = json.loads(response.content)
+        if 'error_code' in result:
+            return False, result
+        result = self._get_result_list(result['list'])
+        return True, result
+
+    def search_file(self, keyword, path="/", re=1, method='search'):
+        path = self.base_path + path
+        params = {"method": method,
+                  "access_token": self.oauth_info['access_token'],
+                  "path": path,
+                  "wd": keyword,
+                  "re": re}
+        response = requests.get(self.base_url,
+                                params=params)
+        result = json.loads(response.content)
+        if 'error_code' in result:
+            return False, result
+        result = self._get_result_list(result['list'])
+        return True, result
+
+    def _get_download_info(self, path):
+        return "https://d.pcs.baidu.com/rest/2.0/pcs/file?" \
+               "method=download&access_token={access_token}" \
+               "&path={path}".format(access_token=self.oauth_info['access_token'],
+                                     path=path)
+
+    def _get_result_list(self, file_list):
+        for i, val in enumerate(file_list):
+            val['download'] = self._get_download_info(val['path'])
+            val['name'] = (val['path']).split('/')[-1]
+            file_list[i] = val
+        return file_list
+
+
+if __name__ == "__main__":
+    oauth_info = utils.read_oauth_info()
+    if not utils.oauth_check(oauth_info):
+        client_id = utils.get_config("apikey")
+        oa_url = "http://openapi.baidu.com/oauth/2.0/authorize?client_id={client_id}" \
+                 "&response_type=token&redirect_uri=oob" \
+                 "&scope=netdisk".format(client_id=client_id)
+        oa_result_base = utils.get_config("oa_result")
+        oauth_result = oauth_ui.get_oa_info(oa_url, oa_result_base)
+        oauth_info = utils.get_oauth_result(oauth_result)
+        if not utils.oauth_check(oauth_info):
+            print(oauth_info)
+            print("登录错误")
+            exit()
+        utils.save_oauth_info(oauth_info)
+
+    print("获取数据成功")
+    bdy = BaiduYun(oauth_info)
+
+    utils.menu(bdy)
+
---
+++ bdy/utils.py
@@ -0,0 +1,136 @@
+# coding: utf8
+##
+# @file utils.py
+# @brief 
+# @author scusjs@foxmail.com
+# @version 0.1.00
+# @date 2016-12-28
+from __future__ import print_function
+import os
+import sys
+import json
+import requests
+from prettytable import PrettyTable
+
+if sys.version_info[0] < 3:
+    from ConfigParser import ConfigParser
+    import urlparse
+    bytes = str
+    str = unicode
+else:
+    from configparser import ConfigParser
+    from urllib import parse as urlparse
+    raw_input = input
+
+
+def get_config(which):
+    config = ConfigParser()
+    path = os.path.join(os.path.dirname(__file__),
+                        'config.ini')
+    config.read(path)
+    return config.get("bdy", which)
+
+
+def get_oauth_result(url):
+    url = url.replace("#", "?", 1)
+    return dict(urlparse.parse_qsl(url))
+
+
+def save_oauth_info(oauth_info):
+    with open('user_access_token', 'w') as f:
+        json.dump(oauth_info, f)
+        
+
+def read_oauth_info():
+    oauth_info = {}
+    try:
+        with open('user_access_token', 'r') as f:
+            oauth_info = json.load(f)
+    except:
+        pass
+    return oauth_info
+
+
+def oauth_check(oauth_info):
+    if len(oauth_info) != 5 or 'scope' not in oauth_info \
+           or "netdisk" not in oauth_info['scope'] \
+           or 'access_token' not in oauth_info:
+        return False
+    url = "https://openapi.baidu.com/rest/2.0/passport/users/getLoggedInUser" \
+          "?access_token={access_token}".format(access_token=oauth_info['access_token'])
+    user_info = requests.get(url).content
+    print(user_info)
+    if 'error_code' in json.loads(user_info):
+        return False
+    return True
+
+
+def get_size_in_nice_string(size_in_bytes):
+    for (cutoff, label) in [(1024*1024*1024, "GB"),
+                            (1024*1024, "MB"),
+                            (1024, "KB"),
+                            ]:
+        if size_in_bytes >= cutoff:
+            return "%.1f %s" % (size_in_bytes * 1.0 / cutoff, label)
+
+    if size_in_bytes == 1:
+        return "1 byte"
+    else:
+        bytes = "%.1f" % (size_in_bytes or 0,)
+        return (bytes[:-2] if bytes.endswith('.0') else bytes) + ' bytes'
+
+
+file_list = []
+
+
+def menu(bdy):
+    global file_list
+    file_list = []
+    while True:
+        user_input = raw_input("请输入命令:")
+        if user_input == "l":
+            request_flag, file_list_tmp = bdy.listDir()
+            show_table(request_flag, file_list_tmp)
+
+        elif user_input == "q":
+            exit()
+        elif user_input == "d":
+            try:
+                user_input = input("请输入待下载序列号:")
+                if user_input >= len(file_list) or user_input < 0:
+                    raise Exception()
+            except:
+                print("请输入正确的数字序号\n")
+                continue
+            print("下载地址为：")
+            print(file_list[user_input]['download'])
+        elif user_input == "s":
+            user_input = raw_input("请输入查询文件关键字：")
+            request_flag, file_list_tmp = bdy.searchFile(user_input)
+            show_table(request_flag, file_list_tmp)
+        else:
+            print("\nh 帮助\nl 列出文件列表\nd 获取下载地址\ns 搜索文件\nq 退出\n")
+
+
+def show_table(request_flag, file_list_tmp):
+    table = PrettyTable(["序列号", "文件/目录名", "文件大小"])
+    if not request_flag:
+        print(file_list_tmp)
+        return
+    global file_list
+    file_list = file_list_tmp
+    table.padding_width = 1
+    for i, val in file_list:
+        table.add_row([i, val['name']], get_size_in_nice_string(val['size']))
+    print(table)
+
+
+if __name__ == "__main__":
+    url = "http://openapi.baidu.com/oauth/2.0/login_success#expires_in=2592000" \
+          "&access_token=23.1890fb4a4b30beaf3cf0f248b5a28a2f.2592000.1485506843.3325487139-9136836" \
+          "&session_secret=c3722940549193100b76a0cf540b23d7" \
+          "&session_key=9mtqVJioDUmdeS50li0pEhXJmEqXM7h4DE9OMWh22mIPRMnNkCxan+5XKtYmvGxAI5wf3tAWWg" \
+          "SXKiKfoPBtZ4jhypKP+1/IXg==&scope=basic"
+    save_oauth_info(get_oauth_result(url))
+    user_info = read_oauth_info()
+    print(oauth_check(user_info))
